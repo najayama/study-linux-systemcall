a05c4552e582fe3a79a632274493c5b2bdc48a84
najayama <27891506+najayama@users.noreply.github.com>
2018-04-07 17:27:49 +0900

    fix bug in readelf

--- elf/readelf.c
+++ elf/readelf.c
@@ -12,23 +12,22 @@ int main(){
         return 0;
     }
 
-
+//read elf headers from simple.out
     fread(&elfhdr.e_ident, sizeof(unsigned char), EI_NIDENT, fp);
-    fread(&elfhdr.e_type, sizeof(Elf64_Quarter), 1, fp);
-    fread(&elfhdr.e_machine, sizeof(Elf64_Quarter), 1, fp);
+    fread(&elfhdr.e_type, sizeof(Elf64_Versym), 1, fp);
+    fread(&elfhdr.e_machine, sizeof(Elf64_Versym), 1, fp);
     fread(&elfhdr.e_version, sizeof(Elf64_Half), 1, fp);
     fread(&elfhdr.e_entry, sizeof(Elf64_Addr), 1, fp);
     fread(&elfhdr.e_phoff, sizeof(Elf64_Off), 1, fp);
     fread(&elfhdr.e_shoff, sizeof(Elf64_Off), 1, fp);
     fread(&elfhdr.e_flags, sizeof(Elf64_Half), 1, fp);
-    fread(&elfhdr.e_ehsize, sizeof(Elf64_Quarter), 1, fp);
-    fread(&elfhdr.e_phentsize, sizeof(Elf64_Quarter), 1, fp);
-    fread(&elfhdr.e_phnum, sizeof(Elf64_Quarter), 1, fp);
-    fread(&elfhdr.e_shentsize, sizeof(Elf64_Quarter), 1, fp);
-    fread(&elfhdr.e_shnum, sizeof(Elf64_Quarter), 1, fp);
-    fread(&elfhdr.e_shstrndx, sizeof(Elf64_Quarter), 1, fp);
+    fread(&elfhdr.e_ehsize, sizeof(Elf64_Versym), 1, fp);
+    fread(&elfhdr.e_phentsize, sizeof(Elf64_Versym), 1, fp);
+    fread(&elfhdr.e_phnum, sizeof(Elf64_Versym), 1, fp);
+    fread(&elfhdr.e_shentsize, sizeof(Elf64_Versym), 1, fp);
+    fread(&elfhdr.e_shnum, sizeof(Elf64_Versym), 1, fp);
+    fread(&elfhdr.e_shstrndx, sizeof(Elf64_Versym), 1, fp);
 
-  //  fread(&elfhdr.e_, sizeof(Elf64_), 1, fp);
 
 
 
